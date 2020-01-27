aca76d6b1760000615a006009bdc4fd01e92068f
Umesh Shrestha <umesh.state@gmail.com>
2020-01-26 21:44:48 -0600

    updated

--- script.js
+++ script.js
@@ -1,91 +1,21 @@
-window.addEventListener('load', function() {
-    var plength = prompt("How many characters would you like your password to be?");
+var length = prompt("Please choose your length of password!")
+var cuppercase = alert("Do you like to use uppercase letters?");
+var clowercase = alert("Do you like to use lowercase letters?");
+var cnumbers = alert("Do you like to use numbers?");
+var csymbols = alert("Do you like to use special characters?");
 
-    while (plength < 8 || plength > 128) {
-        plength = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+
+
+var char = ("0123456789!@#$%^&*()_+{}[];<>?/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+function generate() {
+    var password = "";
+
+    for (let index = 0; index <= 10; index++) {
+        var c = Math.floor((Math.random() * char.length) + 0);
+        password += char.charAt(c);
+
     }
-    var cuppercase = confirm("Would you like to use uppercase letters?");
-    var clowercase = confirm("Would you like to use lowercase letters?");
-    var cnumbers = confirm("would you like to use numbers?");
-    var csymbols = confirm("would you like to use special characters?");
-
-    while (!(cuppercase || clowercase || cnumbers || csymbols)) {
-        alert("You must select at least one character type!");
-
-        cuppercase = confirm("Would you like to use uppercase letters?");
-        clowercase = confirm("Would you like to use lowercase letters?");
-        cnumbers = confirm("would you like to use numbers?");
-        csymbols = confirm("would you like to use special characters?");
-    }
-    const resultEl = document.getElementById('password');
-
-    document.getElementById('generate').addEventListener('click', () => {
-        resultEl.value = generatePassword(clowercase, cuppercase, cnumbers, csymbols, plength);
-    });
-
-    document.getElementById('clipboard').addEventListener('click', () => {
-        const textarea = document.createElement('textarea');
-        const password = resultEl.value;
-
-        if (!password) {
-            return;
-        }
-
-        textarea.value = password;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        textarea.remove();
-        alert('Password copied to clipboard');
-    });
-});
-
-
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-};
-
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = '';
-    const typesCount = lower + upper + number + symbol;
-    const typesArr = [{
-        lower
-    }, {
-        upper
-    }, {
-        number
-    }, {
-        symbol
-    }].filter(item => Object.values(item)[0]);
-
-
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        });
-    }
-
-    const finalPassword = generatedPassword.slice(0, length);
-
-    return finalPassword;
-}
-
-function getRandomLower() {
-    return rando("abcdefghijklmnopqrstuvwxyz")
-}
-
-function getRandomUpper() {
-    return rando("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-}
-
-function getRandomNumber() {
-    return rando(9);
-}
-
-function getRandomSymbol() {
-    return rando("!@#$%^&*()_{}[].<>/?|");
+    console.log(password);
+    document.getElementById("card-body").innerHTML = `<h1>${password}</h1>`
 }
